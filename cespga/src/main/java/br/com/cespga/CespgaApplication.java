bfd0e8d07a504029192f6f29a110369c9f589421
Cardoso21 <wagnerjcardoso21@gmail.com>
2023-12-08 11:37:57 -0300

    criando docker

---
+++ cespga/src/main/java/br/com/cespga/CespgaApplication.java
@@ -0,0 +1,13 @@
+package br.com.cespga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CespgaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CespgaApplication.class, args);
+	}
+
+}
---
+++ cespga/src/test/java/br/com/cespga/CespgaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cespga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CespgaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
